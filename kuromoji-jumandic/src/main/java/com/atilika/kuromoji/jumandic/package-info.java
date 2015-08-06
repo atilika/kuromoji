a2c0959070f9c7bfb1a24af7d17bf51e4e9b1c00
Christian Moen <christian@atilika.com>
2015-08-06 15:43:04 +0900

    Added package level javadoc for tokenizers

---
+++ kuromoji-jumandic/src/main/java/com/atilika/kuromoji/jumandic/package-info.java
@@ -0,0 +1,16 @@
+/**
+ * A Japanese morphological analyzer based on the JUMAN dictionary.
+ * <p>
+ * The following token features are supported:
+ * <ul>
+ * <li>surface form (表層形)
+ * <li>part of speech level 1 (品詞細分類1)
+ * <li>part of speech level 2 (品詞細分類2)
+ * <li>part of speech level 3 (品詞細分類3)
+ * <li>part of speech level 4 (品詞細分類4)
+ * <li>base form (基本形)
+ * <li>reading (読み)
+ * <li>semantic information (代表表記)
+ * </ul>
+ */
+package com.atilika.kuromoji.jumandic;
---
+++ kuromoji-naist-jdic/src/main/java/com/atilika/kuromoji/naist/jdic/package-info.java
@@ -0,0 +1,20 @@
+/**
+ * A Japanese morphological analyzer based on the NAIST-jdic dictionary.
+ * <p>
+ * The following token features are supported:
+ * <ul>
+ * <li>surface form (表層形)
+ * <li>part of speech level 1 (品詞細分類1)
+ * <li>part of speech level 2 (品詞細分類2)
+ * <li>part of speech level 3 (品詞細分類3)
+ * <li>part of speech level 4 (品詞細分類4)
+ * <li>pronunciation (発音)
+ * <li>conjugation type (活用型)
+ * <li>conjugation form (活用形)
+ * <li>base form (基本形)
+ * <li>reading (読み)
+ * <li>transcription variation (表記ゆれ)
+ * <li>compound information (複合語情報)
+ * </ul>
+ */
+package com.atilika.kuromoji.naist.jdic;
