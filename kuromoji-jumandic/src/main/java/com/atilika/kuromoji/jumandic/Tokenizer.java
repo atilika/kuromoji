00d120cf85086d05022e36135a62acdea02ddee0
gerryhocks <gerrard@clockworkmoon.com>
2015-08-21 09:17:15 +0100

    Fix formatting of javadoc and grammar of japanese example sentence

--- kuromoji-jumandic/src/main/java/com/atilika/kuromoji/jumandic/Tokenizer.java
+++ kuromoji-jumandic/src/main/java/com/atilika/kuromoji/jumandic/Tokenizer.java
@@ -1,19 +1,20 @@
 /**
  * Copyright © 2010-2015 Atilika Inc. and contributors (see CONTRIBUTORS.md)
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.  A copy of the
  * License is distributed with this work in the LICENSE.md file.  You may
  * also obtain a copy of the License from
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
 package com.atilika.kuromoji.jumandic;
 
 import com.atilika.kuromoji.AbstractTokenizer;
@@ -27,16 +28,16 @@ import java.util.List;
 
 /**
  * A tokenizer based on the JUMAN DIC dictionary
- * <p/>
+ * <p>
  * See {@link Token} for details on the morphological features produced by this tokenizer
- * <p/>
+ * <p>
  * The following code example demonstrates how to use the Kuromoji tokenizer:
  * <pre>{@code
  * package com.atilika.kuromoji.example;
  * import com.atilika.kuromoji.jumandic.Token;
  * import com.atilika.kuromoji.jumandic.Tokenizer;
  * import java.util.List;
- * <p/>
+ * <p>
  * public class KuromojiExample {
  *     public static void main(String[] args) {
  *         Tokenizer tokenizer = new Tokenizer() ;
@@ -60,7 +61,7 @@ public class Tokenizer extends AbstractTokenizer {
 
     /**
      * Construct a customized tokenizer
-     * <p/>
+     * <p>
      * See {@see com.atilika.kuromoji.jumandic.Tokenizer#Builder}
      */
     private Tokenizer(Builder builder) {
@@ -69,10 +70,10 @@ public class Tokenizer extends AbstractTokenizer {
 
     /**
      * Tokenizes the provided text and returns a list of tokens with various feature information
-     * <p/>
+     * <p>
      * This method is thread safe
      *
-     * @param text text to tokenize
+     * @param text  text to tokenize
      * @return list of Token, not null
      */
     @Override
