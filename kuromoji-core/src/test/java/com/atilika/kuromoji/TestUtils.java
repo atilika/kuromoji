aa61e9c9167d7d41bf5b6e27dd9e3d2af7fa2eb7
Christian Moen <christian@atilika.com>
2015-08-22 16:29:40 +0900

    Fix javadoc

--- kuromoji-core/src/test/java/com/atilika/kuromoji/TestUtils.java
+++ kuromoji-core/src/test/java/com/atilika/kuromoji/TestUtils.java
@@ -1,13 +1,13 @@
 /**
  * Copyright © 2010-2015 Atilika Inc. and contributors (see CONTRIBUTORS.md)
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.  A copy of the
  * License is distributed with this work in the LICENSE.md file.  You may
  * also obtain a copy of the License from
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
