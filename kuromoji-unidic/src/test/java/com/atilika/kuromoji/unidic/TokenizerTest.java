5612d1c5e81b060119ee559a4d04069d59f766b8
gerryhocks <gerrard@clockworkmoon.com>
2015-09-29 21:23:53 +0900

    Remove Instruction class

--- kuromoji-unidic/src/test/java/com/atilika/kuromoji/unidic/TokenizerTest.java
+++ kuromoji-unidic/src/test/java/com/atilika/kuromoji/unidic/TokenizerTest.java
@@ -1,13 +1,13 @@
 /**
  * Copyright © 2010-2015 Atilika Inc. and contributors (see CONTRIBUTORS.md)
- * <p/>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.  A copy of the
  * License is distributed with this work in the LICENSE.md file.  You may
  * also obtain a copy of the License from
- * <p/>
- * http://www.apache.org/licenses/LICENSE-2.0
- * <p/>
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
  * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
@@ -36,17 +36,6 @@ public class TokenizerTest {
     @BeforeClass
     public static void setUpBeforeClass() throws Exception {
         tokenizer = new Tokenizer();
-    }
-
-    @Test
-    public void testNumberThing() {
-        List<Token> tokens = tokenizer.tokenize("七五三");
-        String expectedFeatures = "補助記号,一般,*,*,*,*,,¡,¡,,¡,,記号,*,*,*,*";
-
-        for (Token token : tokens) {
-            System.out.println(token.getSurface() + "\t" + tokens.get(0).getAllFeatures());
-        }
-        assertEquals(expectedFeatures, tokens.get(0).getAllFeatures());
     }
 
     @Test
