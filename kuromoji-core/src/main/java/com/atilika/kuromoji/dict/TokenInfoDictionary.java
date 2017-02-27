d0700ab6dd489aaf0fcb1e4e78ce2f682be9f255
Christian Moen <christian@atilika.com>
2017-02-27 21:40:33 +0900

    Update copyright years

--- kuromoji-core/src/main/java/com/atilika/kuromoji/dict/TokenInfoDictionary.java
+++ kuromoji-core/src/main/java/com/atilika/kuromoji/dict/TokenInfoDictionary.java
@@ -1,5 +1,5 @@
 /**
- * Copyright © 2010-2015 Atilika Inc. and contributors (see CONTRIBUTORS.md)
+ * Copyright © 2010-2017 Atilika Inc. and contributors (see CONTRIBUTORS.md)
  *
  * Licensed under the Apache License, Version 2.0 (the "License"); you may
  * not use this file except in compliance with the License.  A copy of the
